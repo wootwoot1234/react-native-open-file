40fb366c8719cf655fbd04b91294eeda691ba5a0
Tom <tkrones@gmail.com>
2017-03-17 08:29:35 -0400

    Don't show the status bar

--- ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.m
+++ ios/RNDocumentInteractionController/RNDocumentInteractionController/RNDocumentInteractionController.m
@@ -17,14 +17,17 @@ RCT_EXPORT_METHOD(open: (NSURL *)path)
 {
     UIDocumentInteractionController *interactionController = [UIDocumentInteractionController interactionControllerWithURL:path];
     interactionController.delegate = self;
-    [interactionController presentPreviewAnimated:YES];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        [interactionController presentPreviewAnimated:YES];
+    });
+    [[UIApplication sharedApplication] setStatusBarHidden:YES];
 }
 
 - (UIViewController *) documentInteractionControllerViewControllerForPreview: (UIDocumentInteractionController *) controller
 {
+    [[[[UIApplication sharedApplication] delegate] window] setWindowLevel:UIWindowLevelStatusBar];
+    
     return [[[[UIApplication sharedApplication] delegate] window] rootViewController];
 }
 
-
 @end
-
